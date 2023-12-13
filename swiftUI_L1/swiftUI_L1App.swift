838dce1fdbdb2397e1989d5aa7aaa26b3ae730a4
Fawaz <fawazahmed.tahir@systemsltd.com>
2023-12-13 11:51:58 +0500

    Initial Commit

---
+++ swiftUI_L1/swiftUI_L1App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_L1App.swift
+//  swiftUI_L1
+//
+//  Created by Fawaz Ahmed Tahir on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_L1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
