7914dc7a0ee5c0fe188fc504e310d10c5661887d
Fawaz <fawazahmed.tahir@systemsltd.com>
2023-12-19 02:33:02 +0500

    Assignment_1

--- swiftUI_L1/Login.swift
+++ swiftUI_L1/Login.swift
@@ -1,18 +1,96 @@
-//
-//  SwiftUIView.swift
-//  swiftUI_L1
-//
-//  Created by Fawaz Ahmed Tahir on 18/12/2023.
-//
-
 import SwiftUI
 
-struct SwiftUIView: View {
+struct Login: View {
+    @State private var name: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isChecked: Bool = false
+    @State private var isPasswordVisible = false
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing:15) {
+                
+                Text("Log In")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                Spacer().frame(height: 15)
+               
+                TextField("Email", text: $email)
+                    .font(.body)
+                    .font(.system(size: 17, weight: .thin))
+                    .frame(maxWidth: 320, maxHeight: 30)
+                    .foregroundColor(.black)
+                    .frame(height: 44)
+                    .padding(.horizontal, 12)
+                    .background(Color(hex: "F6F6F6"))
+                    .cornerRadius(4.0)
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(Color.gray, lineWidth: 0.2)
+                        )
+//                Spacer().frame(height: 20)
+               
+                ZStack(alignment: .trailing) {
+                           if isPasswordVisible {
+                               TextField("Password", text: $password)
+                                   .font(.body)
+                                   .font(.system(size: 17, weight: .thin))
+                                   .frame(maxWidth: 320, maxHeight: 30)
+                                   .foregroundColor(.black)
+                                   .padding(.horizontal, 12)
+                                   .frame(height: 44)
+                                   .background(
+                                   RoundedRectangle(cornerRadius: 4.0)
+                                    .stroke(Color.gray, lineWidth: 0.2)
+                                       .background(Color(hex: "F6F6F6"))
+                                                       )
+                                   .textContentType(.password)
+                           } else {
+                               SecureField("Password", text: $password)
+                                   .font(.body)
+                                   .font(.system(size: 17, weight: .thin))
+                                   .frame(maxWidth: 320, maxHeight: 30)
+                                   .foregroundColor(.black)
+                                   .padding(.horizontal, 12)
+                                   .frame(height: 44)
+                                   .background(
+                                   RoundedRectangle(cornerRadius: 4.0)
+                                    .stroke(Color.gray, lineWidth: 0.2)
+                                       .background(Color(hex: "F6F6F6"))
+                                   )
+                           }
+                           
+                           Button(action: {
+                               isPasswordVisible.toggle()
+                           }) {
+                               Text(isPasswordVisible ? "Hide" : "Show")
+                               .font(.callout)
+                               .fontWeight(.medium)
+                               .foregroundColor(Color(hex: "5DB075"))
+                               .padding(.trailing,12)
+                           }
+                       }
+                
+                Spacer().frame(height: 15)
+                Button(action: {}) {
+                    Text("Log In")
+                        .fontWeight(.medium)
+                        .font(.body)
+                        .frame(maxWidth: 320, maxHeight: 30)
+                        .padding()
+                        .frame(height: 50)
+                        .foregroundColor(.white)
+                        .background(Color(hex: "5DB075"))
+                        .cornerRadius(40)
+                }
+            }
+            .padding(.bottom,180)
+            .ignoresSafeArea(.keyboard, edges: .bottom)
+        }
     }
 }
 
-#Preview {
-    SwiftUIView()
-}
+
--- swiftUI_L1/SignUp.swift
+++ swiftUI_L1/SignUp.swift
@@ -5,6 +5,10 @@ struct SignUp: View {
     @State private var email: String = ""
     @State private var password: String = ""
     @State private var isChecked: Bool = false
+    @State private var isEmailValid: Bool = true
+    @State private var isPasswordValid: Bool = true
+    @State private var isPasswordVisible = false
+
     
     var body: some View {
         NavigationView {
@@ -14,19 +18,23 @@ struct SignUp: View {
                     .font(.title)
                     .fontWeight(.semibold)
                     .multilineTextAlignment(.center)
+//                    .padding(.top,10)
+                
                 Spacer().frame(height: 15)
                 TextField("Name", text: $name)
                     .font(.body)
                     .frame(maxWidth: 320, maxHeight: 30)
                     .foregroundColor(.black)
+                    .autocapitalization(.words)
                     .frame(height: 44)
                     .padding(.horizontal, 12)
                     .background(Color(hex: "F6F6F6"))
+                    
                     .overlay(
                             RoundedRectangle(cornerRadius: 4)
-                                .stroke(Color.gray, lineWidth: 0.2)
-                        )
-//                Spacer().frame(height: 20)
+                    .stroke(Color.gray, lineWidth: 0.2)
+                            )
+
                 TextField("Email", text: $email)
                     .font(.body)
                     .font(.system(size: 17, weight: .thin))
@@ -38,36 +46,49 @@ struct SignUp: View {
                     .cornerRadius(4.0)
                     .overlay(
                             RoundedRectangle(cornerRadius: 4)
-                                .stroke(Color.gray, lineWidth: 0.2)
-                        )
-//                Spacer().frame(height: 20)
-                TextField("Password", text: $password)
-                    .font(.body)
-                    .font(.system(size: 17, weight: .thin))
-                    .frame(maxWidth: 320, maxHeight: 30)
-                    .foregroundColor(.black)
-                    .padding(.horizontal, 12)
-                    .frame(height: 44)
-                    .background(Color(hex: "F6F6F6"))
-                    .cornerRadius(4.0)
-                    .overlay(
-                        ZStack {
-                            HStack {
-                                Spacer()
-                                Button(action: {
-                                    // Action for the button (if needed)
-                                }) {
-                                    Text("Show")
-                                        .font(.callout)
-                                        .fontWeight(.medium)
-                                        .foregroundColor(Color(hex: "5DB075"))
-                                }
-                                .padding(.trailing, 12)
-                                
-                            }
-                        }
-                        
-                    )
+                    .stroke(Color.gray, lineWidth: 0.2)
+                            )
+
+                ZStack(alignment: .trailing) {
+                           if isPasswordVisible {
+                               TextField("Password", text: $password)
+                                   .font(.body)
+                                   .font(.system(size: 17, weight: .thin))
+                                   .frame(maxWidth: 320, maxHeight: 30)
+                                   .foregroundColor(.black)
+                                   .padding(.horizontal, 12)
+                                   .frame(height: 44)
+                                   .background(
+                                   RoundedRectangle(cornerRadius: 4.0)
+                                    .stroke(Color.gray, lineWidth: 0.2)
+                                       .background(Color(hex: "F6F6F6"))
+                                                       )
+                                   .textContentType(.password)
+                           } else {
+                               SecureField("Password", text: $password)
+                                   .font(.body)
+                                   .font(.system(size: 17, weight: .thin))
+                                   .frame(maxWidth: 320, maxHeight: 30)
+                                   .foregroundColor(.black)
+                                   .padding(.horizontal, 12)
+                                   .frame(height: 44)
+                                   .background(
+                                   RoundedRectangle(cornerRadius: 4.0)
+                                    .stroke(Color.gray, lineWidth: 0.2)
+                                       .background(Color(hex: "F6F6F6"))
+                                   )
+                           }
+                           
+                           Button(action: {
+                               isPasswordVisible.toggle()
+                           }) {
+                               Text(isPasswordVisible ? "Hide" : "Show")
+                               .font(.callout)
+                               .fontWeight(.medium)
+                               .foregroundColor(Color(hex: "5DB075"))
+                               .padding(.trailing,12)
+                           }
+                       }
                 Spacer().frame(height: 2)
                 HStack{
                     Button(action: {
@@ -78,6 +99,12 @@ struct SignUp: View {
                                 .frame(width: 15, height: 15) // Adjust the size as needed
                                 .foregroundColor(isChecked ? Color(hex: "5DB075") : Color(hex: "E8E8E8"))
                                 .cornerRadius(4) // Adjust the corner radius as needed
+                                .overlay(
+                                RoundedRectangle(cornerRadius: 4)
+                                    .stroke(Color(hex: "F6F6F6"), lineWidth: 0.2) // Adjust the stroke color and width as needed
+                            )
+                             
+                                    
                             
                             if isChecked {
                                 Image(systemName: "checkmark")
@@ -94,10 +121,11 @@ struct SignUp: View {
                         .fontWeight(.regular)
                         .foregroundColor(Color(.gray))
                         .lineSpacing(-10)
+                        .lineLimit(2) // Set the maximum number of lines to 2
+                        .fixedSize(horizontal: false, vertical: true) // Allow vertical expansion
                         .kerning(-1)
                         .multilineTextAlignment(.leading)
                         .padding(.leading, 5)
-                    //                       .padding(.vertical, 8)
                     Spacer()
                 }
                 
@@ -128,32 +156,26 @@ struct SignUp: View {
                         Image(systemName: "xmark")
                             .foregroundColor(.gray)
                             .imageScale(.small)
-                            .padding(.leading,5)
-                            .padding(.top,25)
+                            .padding(.leading,10)
+                            .padding(.top,12)
                     },
-                    trailing: NavigationLink(destination: LoginView()) {
+                    trailing: NavigationLink(destination: Login()) {
                         Text("Login")
                             .font(.callout)
                             .fontWeight(.medium)
                             .foregroundColor(Color(hex:"5DB075"))
                             .padding(.trailing,10)
-                            .padding(.top,25)
+                            .padding(.top,12)
 
                     }
                 )
-            }.padding(.top,-310)
+            }
+            
+
         }
+        .padding(.bottom,330)
 
-    }
-}
-
-struct LoginView: View {
-    var body: some View {
-        // Your login view content goes here
-        Text("Log In")
-            .font(.title)
-            .fontWeight(.semibold)
-            .multilineTextAlignment(.center)
+        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
 }
 
@@ -163,29 +185,6 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let a, r, g, b: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (a, r, g, b) = (1, 1, 1, 0)
-        }
 
-        self.init(
-            .sRGB,
-            red: Double(r) / 255,
-            green: Double(g) / 255,
-            blue:  Double(b) / 255,
-            opacity: Double(a) / 255
-        )
-    }
-}
+
+
